78f8daa1ef12a82d889ecbe02ccedcddc8c4db5d
Richard Melchior <raustin.melchior@gmail.com>
2018-06-02 20:14:57 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-  array.join(", ")
+  case (array.length)
+  when 1
+    return array;
+  when 2
+    return array.join(" and ")
+  else
+    before_last = array.pop();
+    return "#{array.join(before_last)} and #{array.last}";
+  end
+  #array.join(", ")
 end
